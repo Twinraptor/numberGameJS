55243c3f50ea89500181d1f0629996579e2d53dc
twinraptor <mythosx@hotmail.com>
2020-09-20 15:17:29 -0700

    completed exercise

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+alert("Hello, this is alert");
+
+var age = prompt("What is your age?");
+document.getElementById("test").innerHTML = "Your age is:" + age;
+
+//Numbers lesson
+
+var num1 = 5.7;
+num1 = num1 + 1;
+console.log(num1);
